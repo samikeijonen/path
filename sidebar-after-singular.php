6da95f2dc4c4ad7dbc559de12f14db6897db734c
Sami Keijonen <sami.keijonen@foxnet.fi>
2012-07-09 17:32:17 +0300

    Hooks added.

--- sidebar-after-singular.php
+++ sidebar-after-singular.php
@@ -11,10 +11,18 @@
 
 if ( is_active_sidebar( 'after-singular' ) ) : ?>
 
+	<?php do_atomic( 'before_sidebar_after_singular' ); // path_before_sidebar_after_singular ?>
+
 	<div id="sidebar-after-singular" class="sidebar">
+	
+		<?php do_atomic( 'open_sidebar_after_singular' ); // path_open_sidebar_after_singular ?>
 
 		<?php dynamic_sidebar( 'after-singular' ); ?>
+		
+		<?php do_atomic( 'close_sidebar_after_singular' ); // path_close_after_singular ?>
 
 	</div><!-- #sidebar-after-singular -->
+	
+	<?php do_atomic( 'after_sidebar_after_singular' ); // path_after_sidebar_after_singular ?>
 
 <?php endif; ?>
