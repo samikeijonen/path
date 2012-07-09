0bbde476f723b3053827260de49ca417605b2233
Sami Keijonen <sami.keijonen@foxnet.fi>
2012-07-09 17:30:25 +0300

    Hooks added.

--- sidebar-before-content.php
+++ sidebar-before-content.php
@@ -10,15 +10,23 @@
  */
 
 if ( is_active_sidebar( 'before-content' ) ) : ?>
+	
+	<?php do_atomic( 'before_sidebar_before_content' ); // path_before_sidebar_before_content ?>
 
 	<div id="sidebar-before-content" class="sidebar">
+	
+		<?php do_atomic( 'open_sidebar_before_content' ); // path_open_sidebar_before_content ?>
 		
 		<div class="wrap">
 
 			<?php dynamic_sidebar( 'before-content' ); ?>
 			
 		</div><!-- .wrap -->
+		
+		<?php do_atomic( 'close_sidebar_before_content' ); // path_close_sidebar_before_content ?>
 
 	</div><!-- #sidebar-before-content -->
+	
+	<?php do_atomic( 'after_sidebar_before_content' ); // path_after_sidebar_before_content ?>
 
 <?php endif; ?>
--- sidebar-subsidiary.php
+++ sidebar-subsidiary.php
@@ -11,14 +11,22 @@
 
 if ( is_active_sidebar( 'subsidiary' ) ) : ?>
 
+	<?php do_atomic( 'before_sidebar_subsidiary' ); // path_before_sidebar_subsidiary ?>
+
 	<div id="sidebar-subsidiary" class="sidebar">
+	
+		<?php do_atomic( 'open_sidebar_subsidiary' ); // path_open_sidebar_subsidiary ?>
 		
 		<div class="wrap">
 
 			<?php dynamic_sidebar( 'subsidiary' ); ?>
 			
 		</div><!-- .wrap -->
+		
+		<?php do_atomic( 'close_sidebar_subsidiary' ); // path_close_sidebar_subsidiary ?>
 
 	</div><!-- #sidebar-subsidiary -->
+	
+	<?php do_atomic( 'after_sidebar_subsidiary' ); // path_after_sidebar_subsidiary ?>
 
 <?php endif; ?>
