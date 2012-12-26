9b598a8240bff9cf9f3070ae6ba58b060019fc25
Sami Keijonen <sami.keijonen@foxnet.fi>
2012-12-26 16:55:23 +0200

    Take path golobal layout off.

--- js/customize/path-customizer.js
+++ js/customize/path-customizer.js
@@ -9,11 +9,4 @@
 			$( '#site-description' ).html( to );
 		} );
 	} );
-	wp.customize( 'path_theme_settings[path_global_layout]', function( value ) {
-		value.bind( function( to ) {
-			var classes = $( 'body' ).attr( 'class' ).replace( /layout-[a-zA-Z0-9_-]*/g, '' ); // replace class with prefix layout- to ''
-			$( 'body' ).attr( 'class', classes ).addClass( to ); // add new class
-		} );
-	} );
-
 } )( jQuery );
