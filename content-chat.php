ceeb3c370572aba22b686c4f28328d42a62ee064
Sami Keijonen <sami.keijonen@foxnet.fi>
2013-08-19 22:12:47 +0300

    Chat content added.

---
+++ content-chat.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Chat Content Template
+ *
+ * Template used for 'chat' post format.
+ *
+ * @package Path
+ * @subpackage Template
+ * @since 0.2.1
+ */
+
+do_atomic( 'before_entry' ); // path_before_entry ?>
+
+<article id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
+
+	<?php do_atomic( 'open_entry' ); // path_open_entry ?>
+
+	<?php if ( is_singular() ) { ?>
+
+		<header class="entry-header">	
+			<?php echo apply_atomic_shortcode( 'entry_title', '[entry-title]' ); ?>
+			<?php echo apply_atomic_shortcode( 'byline', '<div class="byline">' . __( '[post-format-link] published on [entry-published] [entry-comments-link before=" | "] [entry-edit-link before=" | "]', 'path' ) . '</div>' ); ?>
+		</header><!-- .entry-header -->
+	
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'path' ), 'after' => '</p>' ) ); ?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+			<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '[entry-terms taxonomy="category" before="Posted in "] [entry-terms before="Tagged "]', 'path' ) . '</div>' ); ?>
+		</footer><!-- .entry-footer -->
+		
+	<?php } else { ?>
+
+		<header class="entry-header">	
+			<?php echo apply_atomic_shortcode( 'entry_title', '[entry-title]' ); ?>
+		</header><!-- .entry-header -->
+		
+		<?php echo do_shortcode( '[gallery numberposts="3" orderby="rand"]' ); ?>
+
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', 'path' ), 'after' => '</p>' ) ); ?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+			<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '[post-format-link] published on [entry-published] [entry-permalink before="| "] [entry-comments-link before="| "] [entry-edit-link before="| "]', 'path' ) . '</div>' ); ?>
+		</footer><!-- .entry-footer -->
+		
+	<?php } ?>
+
+	<?php do_atomic( 'close_entry' ); // path_close_entry ?>
+
+</article><!-- .hentry -->
+
+<?php do_atomic( 'after_entry' ); // path_after_entry ?>
